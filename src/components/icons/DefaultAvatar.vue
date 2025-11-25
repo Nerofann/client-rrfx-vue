9fa65c1b275d9a31c2ffdb3f8555fa7d7e16cb0c
Fani Annur Rozi <crazyhx4@gmail.com>
2025-11-26 00:42:45 +0700

    Add DefaultAvatar component with admin image

---
+++ src/components/icons/DefaultAvatar.vue
@@ -0,0 +1,7 @@
+<script setup>
+import AvatarImg from '@/assets/images/admin.png'
+</script>
+
+<template>
+    <img class="custom-avatar" style="width: 40px; height: 40px;" :src="AvatarImg" alt="image">
+</template>
