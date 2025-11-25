cec34795d72358e7546f05f37aee3ca1415c5b7e
Fani Annur Rozi <crazyhx4@gmail.com>
2025-11-26 00:26:00 +0700

    Refactor token management in useAuth to improve readability and maintainability

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,8 +1,29 @@
 import { useLocalStorage } from "@vueuse/core";
 
-export const saveToken = async ({string: accessToken, string: refreshToken}) => {
-    const token = useLocalStorage('token', { accessToken: null, refreshToken: null });
-    token.value.accessToken = accessToken;
-    token.value.refreshToken = refreshToken;
-    return token;
+const token = useLocalStorage('token', { 
+    accessToken: null, 
+    refreshToken: null 
+});
+
+export const saveToken = (accessToken, refreshToken) => {
+    token.value = {
+        accessToken: accessToken,
+        refreshToken: refreshToken
+    };
+    return token.value;
+}
+
+export const getToken = () => {
+    return token.value;
+}
+
+export const clearToken = () => {
+    token.value = {
+        accessToken: null,
+        refreshToken: null
+    };
+}
+
+export const isAuthenticated = () => {
+    return !!token.value.accessToken;
 }
