e7505fe84fa393ee9b2369316f8cd9e8bea48027
Fani Annur Rozi <crazyhx4@gmail.com>
2025-11-26 17:09:17 +0700

    update

--- src/main.js
+++ src/main.js
@@ -6,17 +6,10 @@ import './assets/vendor/css/sharp-regular.min.css'
 import './assets/vendor/css/jquery-ui.min.css'
 import './assets/vendor/css/OverlayScrollbars.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css' // Bootstrap dari npm
+import 'bootstrap/dist/js/bootstrap.bundle.min.js' // Bootstrap JS
 import './assets/css/style.css'
 import './assets/css/custom.css'
 import './assets/css/gold-color.css'
-
-/** Template JS */
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js' // Bootstrap JS dari npm
-import './assets/js/jquery.min.js'
-import './assets/vendor/js/jquery-ui.min.js'
-import './assets/vendor/js/jquery.overlayScrollbars.min.js'
-import './assets/js/main.js'
-import './assets/js/custom.js'
 
 /** Template Images */
 import './assets/images/logo-rrfx3.png'
