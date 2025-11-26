84ae843d35a24c4d0ac7de40b3f1dcf4a49ee749
Fani Annur Rozi <crazyhx4@gmail.com>
2025-11-27 00:39:50 +0700

    Add user interface definition in user.interface.js

---
+++ src/interface/user.interface.js
@@ -0,0 +1,15 @@
+export const userInterface = () => ({
+    fullname: "",
+    email: "",
+    username: "",
+    created_at: "",
+    user_type: "",
+    place_of_birth: "",
+    date_of_birth: "",
+    address: "",
+    country_id: "",
+    phone: "",
+    phone_code: "",
+    is_verified: false,
+    app_theme: "light"
+})
