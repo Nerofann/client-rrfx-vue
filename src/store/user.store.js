fc2ad1a29552f5b4fe7604bb46ac09b3407c3e2e
Fani Annur Rozi <crazyhx4@gmail.com>
2025-11-27 00:40:08 +0700

    Add user store with fetchUser function for user profile management

--- src/store/user.store.js
+++ src/store/user.store.js
@@ -1,28 +1,30 @@
 import { userProfile } from '@/api/user/user.api.js';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
+import { userInterface } from "@/interface/user.interface";
 
 export const userStore = defineStore("user", () => {
-    const User = ref(null);
-    const loading = ref(false);
+    const User = ref(userInterface());
+    const isLoaded = ref(false);
 
     const fetchUser = async () => {
         // Skip jika sedang loading atau sudah ada data
-        if (loading.value || User.value) return User.value;
-        
-        loading.value = true;
         const data = await userProfile();
         if(data) {
-            User.value = data;
+            User.value = {
+                ...userInterface(),
+                ...data
+            };
+
+            isLoaded.value = true;
         }
         
-        loading.value = false;
         return User.value;
     }
     
     return {
         User,
-        loading,
+        isLoaded,
         fetchUser
     }
 });
