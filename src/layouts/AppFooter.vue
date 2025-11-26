561258b169176c593a5aa4f447f0e10b9e562d05
Fani Annur Rozi <crazyhx4@gmail.com>
2025-11-27 02:34:04 +0700

    Fix AppFooter.vue to set footer position as fixed

--- src/layouts/AppFooter.vue
+++ src/layouts/AppFooter.vue
@@ -3,7 +3,7 @@ const appName = import.meta.env.VITE_APP_NAME || 'App';
 </script>
 
 <template>
-    <div class="footer">
+    <div class="footer" style="position: fixed;">
         <p>Copyright &copy; {{ new Date().getFullYear() }} All Rights Reserved By <span class="text-primary">{{ appName }}</span></p>
     </div>
 </template>
