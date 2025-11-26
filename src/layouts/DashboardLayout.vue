a403506e66856667a38aaaad8d0c21fd22ff3c0e
Fani Annur Rozi <crazyhx4@gmail.com>
2025-11-26 17:10:20 +0700

    Add user composable to DashboardLayout for user state management

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -3,6 +3,10 @@ import { RouterView } from 'vue-router'
 import AppFooter from '@/layouts/AppFooter.vue';
 import AppHeader from '@/layouts/AppHeader.vue';
 import AppSidebar from '@/layouts/AppSidebar.vue';
+import { useUser } from '@/composables/useUser';
+
+const user = useUser();
+console.log(user);
 </script>
 
 <template>
