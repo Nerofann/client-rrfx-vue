42a6f556539cac231e100d2a2af8c75c676ab14d
Fani Annur Rozi <crazyhx4@gmail.com>
2025-11-27 00:40:36 +0700

    Remove user fetching logic from DashboardLayout.vue and update AppHeader component

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -3,20 +3,12 @@ import { RouterView } from 'vue-router'
 import AppFooter from '@/layouts/AppFooter.vue';
 import AppHeader from '@/layouts/AppHeader.vue';
 import AppSidebar from '@/layouts/AppSidebar.vue';
-import { userStore } from '@/store/user';
-import { onMounted } from 'vue';
-
-const store = userStore();
-
-onMounted(async () => {
-    await store.fetchUser();
-    console.log('User loaded:', store.User)
-})
+    
 </script>
 
 <template>
     <div class="main-content login-panel login-panel-2">
-        <AppHeader :user="store.User" />
+        <AppHeader />
         <AppSidebar />
         <RouterView />
         <AppFooter />
