7d4b8cd0b31103419ce72e334826dd66c925dae7
Fani Annur Rozi <crazyhx4@gmail.com>
2025-12-14 13:42:05 +0700

    fix loading screen saat data user dimuat di router

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -3,19 +3,28 @@ import { RouterView } from 'vue-router'
 import AppFooter from '@/layouts/AppFooter.vue';
 import AppHeader from '@/layouts/AppHeader.vue';
 import AppSidebar from '@/layouts/AppSidebar.vue';
-import { onMounted } from 'vue';
+import { computed, onMounted, watch } from 'vue';
+import { useUserStore } from '@/store/user.store';
 
+const store = useUserStore()
+const profileReady = computed(() => store.isLoaded && !store.loading);
 onMounted(() => {
     window.document.body.className = 'body-padding body-p-top light-theme'
 });
 </script>
 
 <template>
-    <AppHeader />
-    <AppSidebar />
-    
-    <div class="main-content">
-        <RouterView />
-        <AppFooter />
+    <div v-if="!profileReady">
+        <p>Loading...</p>
+    </div>
+
+    <div v-else>
+        <AppHeader />
+        <AppSidebar />
+        
+        <div class="main-content">
+            <RouterView />
+            <AppFooter />
+        </div>
     </div>
 </template>
