73222e3e7c49f5105a8bd607f8fca4553c16b87c
Fani Annur Rozi <crazyhx4@gmail.com>
2025-12-14 15:05:15 +0700

    progress halaman dasboard bagian news

---
+++ src/views/dashboard/News.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row mt-2">
+        <div class="col-md-12">
+            <div class="panel">
+                <div class="card p-2">
+                    <div class="text-center">
+                        <h5 class="mb-0">News</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card h-100">
+                <img src="https://picsum.photos/200/300" class="card-img-top" alt="Blog Image">
+                <div class="card-body">
+                    <div class="d-flex flex-column h-100">
+                        <p class="small">News</p>
+                        <div class="mt-auto">
+                            <a href="/news?detail=" class="mt-auto btn btn-sm btn-primary">Detail</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
